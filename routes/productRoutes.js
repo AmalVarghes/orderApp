fff4c89e4e351f35d67c35c5f3746b176a6a2d81
Amal Varghese <amal.inj4@gmail.com>
2026-02-23 08:56:24 +0530

    product url update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const { createProduct, getProducts } = require('../controllers/productController');
+const {
+  createProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+} = require('../controllers/productController');
 
 const router = express.Router();
 
 router.post('/products', createProduct);
 router.get('/products', getProducts);
-router.put('/api/products/:id', updateProduct);      // or patch
-router.patch('/api/products/:id', updateProduct);
-router.delete('/api/products/:id', deleteProduct);
+router.put('/products/:id', updateProduct);
+router.patch('/products/:id', updateProduct);
+router.delete('/products/:id', deleteProduct);
 
 module.exports = router;
